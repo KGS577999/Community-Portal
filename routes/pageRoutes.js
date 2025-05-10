285434f62834256fc47e2bb260cc2f7ca35da44f
KGS577999 <577999@student.belgiumcampus.ac.za>
2025-05-10 14:39:00 +0200

    added class pageheading style to css; added image styling and changed about page styling

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -9,14 +9,14 @@ router.get('/', (req, res) => {
 
 router.get('/about', (req, res) => {
     const team = [
-        {name: 'Kyle', surname: 'Smith', bio: 'Mini Biography 1', image: '/images/pfp1.png'},
-        {name: 'Tiaan', surname: 'Dorfling', bio: 'Mini Biography 2', image: '/images/pfp2.png'},
-        {name: 'Gabriel', surname: 'Botha', bio: 'Mini Biography 3', image: '/images/pfp1.png'},
-        {name: 'Emile', surname: 'Pieterse', bio: 'Mini Biography 4', image: '/images/pfp2.png'}
+        {name: 'Kyle', surname: 'Smith', bio: 'Kyle is passionate about the gaming community and oversees all gaming tournaments. He organizes events, manages registrations, and ensures fair play across all competitions.', image: '/images/pfp1.png'},
+        {name: 'Tiaan', surname: 'Dorfling', bio: 'Tiaan leads our charity fundraising efforts. He connects with local causes, coordinates events, and ensures that every initiative makes a meaningful impact on the community.', image: '/images/pfp2.png'},
+        {name: 'Gabriel', surname: 'Botha', bio: 'Gabriel brings people together through cultural festivals. He curates diverse programs that celebrate traditions, music, art, and cuisine from different cultures.', image: '/images/pfp1.png'},
+        {name: 'Emile', surname: 'Pieterse', bio: 'Emile focuses on strengthening community bonds by organizing neighborhood meetups. His events foster connection, support local talent, and encourage inclusive dialogue.', image: '/images/pfp2.png'}
     ]
 
     const orgBio = {
-        info: 'Our community portal is dedicated to keeping residents informed and engaged with the latest local events, gatherings, and activities. Whether its gaming tournaments, charity fundraisers, cultural festivals, or neighborhood meetups, we serve as the go-to hub for discovering whats happening nearby. We aim to bring people together, foster connections, and celebrate the diverse interests that make our community vibrant. Stay updated, get involved, and be part of the experiences that make our town thrive!'
+        info: 'Our community portal is dedicated to keeping residents informed and engaged with the latest local events, gatherings, and activities. Whether its gaming tournaments, charity fundraisers, cultural festivals, or neighborhood meetups, we serve as the go-to hub for discovering what is happening nearby. We aim to bring people together, foster connections, and celebrate the diverse interests that make our community vibrant. Stay updated, get involved, and be part of the experiences that makes our community thrive!'
         }
 
     res.render('pages/about', {team, orgBio})
