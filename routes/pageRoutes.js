7e67416658d0d266ed093c0adb03e8f325896c54
KGS577999 <577999@student.belgiumcampus.ac.za>
2025-05-10 12:35:16 +0200

    added about page content; changed inbedded nav to dynamic nav (header.js) to about page and home page

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -8,7 +8,18 @@ router.get('/', (req, res) => {
 });
 
 router.get('/about', (req, res) => {
-    res.render('pages/about')
+    const team = [
+        {name: 'Kyle', surname: 'Smith', bio: 'Mini Biography 1', image: '/images/pfp1.png'},
+        {name: 'Tiaan', surname: 'Dorfling', bio: 'Mini Biography 2', image: '/images/pfp2.png'},
+        {name: 'Gabriel', surname: 'Botha', bio: 'Mini Biography 3', image: '/images/pfp1.png'},
+        {name: 'Emile', surname: 'Pieterse', bio: 'Mini Biography 4', image: '/images/pfp2.png'}
+    ]
+
+    const orgBio = {
+        info: 'Our community portal is dedicated to keeping residents informed and engaged with the latest local events, gatherings, and activities. Whether its gaming tournaments, charity fundraisers, cultural festivals, or neighborhood meetups, we serve as the go-to hub for discovering whats happening nearby. We aim to bring people together, foster connections, and celebrate the diverse interests that make our community vibrant. Stay updated, get involved, and be part of the experiences that make our town thrive!'
+        }
+
+    res.render('pages/about', {team, orgBio})
 });
 
 router.get('/events', (req, res) => {
