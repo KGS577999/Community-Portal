6396d6026ee4d16ecdf561370cbc0c6c39be8bf5
Tiaan Dorfling <purnjappie@gmail.com>
2025-05-12 18:25:52 +0200

    added dotenv for port

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 const express = require("express");
 const path = require("path");
 const pageRoutes = require("./routes/pageRoutes");
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3005;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
